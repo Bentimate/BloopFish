b83d65ff22f0b7e7b1938ee628d91a0a0fe7bf07
Benny <benny.ong348@gmail.com>
2022-05-29 15:43:13 +0800

    altered boat movement
    boat turns and moves forward with each key pressed ie.
    A -> simulated rowing at left side of boat, boat turns right and moves forward
    D -> simulated rowing at right side of boat, boat turns left and moves forward

--- Main/Assets/BoatMovement.cs
+++ Main/Assets/BoatMovement.cs
@@ -4,30 +4,31 @@ using UnityEngine;
 
 public class BoatMovement : MonoBehaviour
 {
-    
     public Rigidbody2D boat;
-    private float rowVelocityIncrement = 5;
-    private Vector2 normal = new Vector2(0, 1);
-    // private Vector2 leftVector = new Vector2(5, 1);
-    // private Vector2 rightVector = new Vector2(-5, 1);
+    private Vector2 forward = new Vector2(0, 1);
+    public float speed;
+    public float rotation;
     
     // Start is called before the first frame update
     void Start()
     {
         boat = GetComponent<Rigidbody2D>();
         boat.velocity = new Vector2(0, 0);
-       
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A)) {
-            boat.AddTorque(rowVelocityIncrement);
+            //transform.Rotate(0, 0, rotation);
+            boat.AddTorque(rotation);
+            boat.AddRelativeForce(forward * speed);
         }
 
         if (Input.GetKeyDown(KeyCode.D)) {
-            boat.AddTorque(-rowVelocityIncrement);
+            //transform.Rotate(0, 0, -rotation);
+            boat.AddTorque(-rotation);
+            boat.AddRelativeForce(forward * speed);
         }
     }
 }
